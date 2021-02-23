662fe7e0000b81feee3191549e0f942c5a7b8eba
Ayush Jain <75239620+ayushjaink8@users.noreply.github.com>
2021-02-23 19:14:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
 function generatecolour() {
-  const hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-
-  var hexcolor = "#";
-
-  for (var i = 0; i < 6; i++) {
-    var random = Math.floor(Math.random() * hexArray.length);
-    hexcolor = hexcolor + hexArray[random];
+    const hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  
+    var hexcolor = "#";
+  
+    for (var i = 0; i < 6; i++) {
+      var random = Math.floor(Math.random() * hexArray.length);
+      hexcolor = hexcolor + hexArray[random];
+    }
+  
+    return hexcolor;
   }
+  
+  function hexcolor() {
+    var color = generatecolour();
+    console.log(color);
+    var body = document.querySelector("body");
+    body.style.backgroundColor = color;
+  
+    var changeName2 = document.querySelector(".changeName2");
+    changeName2.textContent=color;
+  
+  }
+  
+  
+  function gradiantcolor(){
+      var color1 = generatecolour();
+      var color2 = generatecolour();
+  
+      console.log(color1+" + "+color2);
+  
+      var body = document.getElementById("body");
+  
+     //body.style.backgroundColor=color1;
 
-  return hexcolor;
-}
-
-function hexcolor() {
-  var color = generatecolour();
-  console.log(color);
-  var body = document.querySelector("body");
-  body.style.backgroundColor = color;
-
-  var changeName2 = document.querySelector(".changeName2");
-  changeName2.textContent=color;
-
-}
-
-
-function gradiantcolor(){
-    var color1 = generatecolour();
-    var color2 = generatecolour();
-
-    console.log(color1+" + "+color2);
-
-    var body = document.getElementById("body");
-
-   body.style.backgroundColor=color1;
-   body.style.backgroundImage = `linear-gradient($(color1) , $(color2))`;
-    var changeName1 = document.querySelector(".changeName1");
-    changeName1.textContent=color1+" + "+color2;
-
-}
-
-
+     body.style.backgroundImage = `linear-gradient(320deg ,${color1} , ${color2})`;
+      var changeName1 = document.querySelector(".changeName1");
+      changeName1.textContent=color1+" + "+color2;
+  
+  }
